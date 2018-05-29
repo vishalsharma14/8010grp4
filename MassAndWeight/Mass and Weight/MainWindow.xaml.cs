c939cf3ef8aa51a200f97d1877274d65567e749a
vishalsharma14 <vishal.sharma@hashedin.com>
2018-05-29 15:48:12 -0400

    Assignment-3 MassAndWeight

---
+++ MassAndWeight/Mass and Weight/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿/** MassAndWeight - A3 - Group4
+ * Vishal V.    8205031
+ * Gevindu M.
+ * Rahul M.     8258980
+ * Ramandeep K.
+ * Gurjinder S.
+ */
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Mass_and_Weight
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MassWeightVM mwVM = new MassWeightVM();
+
+        const int DECIMAL_PLACES_ALLOWED = 3;  // Maximum number of decimal places permitted for input
+        const string DECIMAL_CHAR = ".";
+        const string NUMBER_REGEX = "[0-9]";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = mwVM;
+        }
+
+        // Function to validate input number
+        private void TbInputMass_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            bool isInputTextNumber = new Regex(NUMBER_REGEX).IsMatch(e.Text);
+            bool isKeyDecimal = (e.Text == DECIMAL_CHAR);
+            string inputText = TbInputMass.Text;
+            // isInValidDecimal: true if input is invalid input (has more than 3 decimal numbers), false otherwise
+            bool isInValidDecimal = (TbInputMass.Text.Contains(DECIMAL_CHAR) &&
+                (((inputText.Length - inputText.IndexOf(DECIMAL_CHAR)) > DECIMAL_PLACES_ALLOWED) || isKeyDecimal));
+
+            // if input is not a valid decimal => (e.Hanlded = true)
+            if (!(isInputTextNumber || isKeyDecimal) || isInValidDecimal)
+                e.Handled = true;
+        }
+
+        // Function to handle Text Change Event for TextBox (to disable CalculateWeight button)
+        private void TbInputMass_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            if (TbInputMass.Text != string.Empty)
+            {
+                BtnCalculateWeight.IsEnabled = true;
+            }
+            else
+            {
+                BtnCalculateWeight.IsEnabled = false;
+            }
+        }
+
+        // Function to handle click event for Calculate weight button
+        private void BtnCalculateWeight_Click(object sender, RoutedEventArgs e)
+        {
+            mwVM.ConvertMassToWeight(); // Converts input mass to weight and updates binded value
+        }
+    }
+}
---
+++ MassAndWeight/Mass and Weight/MassWeightVM.cs
@@ -0,0 +1,99 @@
+﻿/** MassAndWeight - A3 - Group4
+ * Vishal V.    8205031
+ * Gevindu M.
+ * Rahul M.     8258980
+ * Ramandeep K.
+ * Gurjinder S.
+ */
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Mass_and_Weight
+{
+    class MassWeightVM : INotifyPropertyChanged
+    {
+        const float CONVERSION_FACTOR = 9.8F;
+
+        // Weight Threshold constants
+        const int HEAVY_OBJECT_WEIGHT_THRESHOLD = 1000;
+        const string HEAVY_OBJECT_MSG = "The object is too heavy!";
+        const int LIGHT_OBJECT_WEIGHT_THRESHOLD = 10;
+        const string LIGHT_OBJECT_MSG = "The object is too light!";
+
+        const string WEIGHT_STRING_PATTERN = "0.## N";  // String pattern to display calculated weight in Newtons
+
+        string weight;
+        string mass;
+        string weightDescription;
+
+        public string Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Mass
+        {
+            get
+            {
+                return mass;
+            }
+            set
+            {
+                mass = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string WeightDescription
+        {
+            get
+            {
+                return weightDescription;
+            }
+            set
+            {
+                weightDescription = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // Function to set weight description
+        private void SetWeightDescription(float calculatedWeight)
+        {
+            WeightDescription = string.Empty;
+
+            if (calculatedWeight > HEAVY_OBJECT_WEIGHT_THRESHOLD)
+            {
+                WeightDescription = HEAVY_OBJECT_MSG;
+            }
+            else if (calculatedWeight < LIGHT_OBJECT_WEIGHT_THRESHOLD)
+            {
+                WeightDescription = LIGHT_OBJECT_MSG;
+            }
+        }
+
+        // Function to calculate weight for input mass
+        public void ConvertMassToWeight()
+        {
+            float calculatedWeight = float.Parse(Mass) * CONVERSION_FACTOR;
+            SetWeightDescription(calculatedWeight);
+            Weight = calculatedWeight.ToString(WEIGHT_STRING_PATTERN);
+        }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Property Change event handler
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
